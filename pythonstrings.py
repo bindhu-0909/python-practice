c352636e4dd10fda2a52855aa00c9bd9404a1026
unknown <bindhu.r2009@gmail.com>
2025-05-15 21:08:04 +0530

    initial commit

---
+++ pythonstrings.py
@@ -0,0 +1,83 @@
+# You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+# a = "Hello"
+# print(a)
+
+# assign a multiline string to a variable by using three quotes:
+
+#  a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# or three single quotes:
+
+
+
+# Square brackets can be used to access elements of the string.
+
+#Strings are Arrays [list to store]
+# a = "Hello, World!"
+# print(a[1])
+
+
+
+# Looping Through a String
+# for x in "banana":
+#   print(x)
+ 
+# we can loop through the characters in a string, with a for loop.
+
+
+
+# String Length
+# To get the length of a string, use the len() function.
+
+# a = "Hello, World!"
+# print(len(a))
+
+# number of characters in the line
+
+
+
+# Check String
+# spelling name and adding a spcecial symbol that proves if spelled right
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+
+
+# Check if NOT
+# to check if a certain phrase or character is NOT present
+
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print("No, 'expensive' is NOT present.")
+
+
+
+# Slicing
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+# total no of chars 12
+# 1st index is 6 because to not print "hello, "
+# aaab
+b = "Hello, World!"
+print(b[-6:0])
+
+
---
+++ variables.py
@@ -0,0 +1,130 @@
+# Variables are containers for storing data values.
+# A variable is created the moment you first assign a value to it.
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4         # x is of type int
+# x = "Sally"  # x is now of type str
+# print(x)
+
+# x = str(3)    # x will be "3"
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# to find data type use (type(-))
+
+# both single and double qoutes are same 
+# x = "john"
+# is same as
+# x = 'john'
+
+#Variable names are case-sensitive.
+
+# a = 4
+# A = "Sally"
+# both are not same 
+
+
+
+# variables have legal and ilegal names
+# names with underscore, letters, and no from 0-9 are legal
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+# names with - , and starting with numbers are ilegal
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+
+# variables are case sensitive 
+
+# having 3types : camel case 
+#       Each word, except the first, starts with a capital letter:
+#      myVariableName = "John"
+# : pascal case
+#       Each word starts with a capital letter:
+#        MyVariableName = "John" 
+# : snake case
+#      Each word is separated by an underscore character:
+#      my_variable_name = "John"
+
+
+
+# variables have multiple values
+#   should be assigned in one line
+#  x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# you can assign the same value to multiple variables
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z) 
+
+#  Unpack a Collection
+
+#  fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# () is used for grouping things or functions
+# [] is used for lists and getting items from a list 
+ 
+
+# In the print() function, you output multiple variables, separated by a comma:
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+#  when you try to combine a string and a number with the + operator, Python will give you an error:
+# x = 5
+# y = "John"
+# print(x + y)
+
+# print() function is to separate them with commas
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+# x = "awesome"
+
+# def myfunc():
+#   print("Python is " + x)
+
+# myfunc()
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
