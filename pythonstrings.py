ec57f14317fa0391a5575e2230cef82f607a5a3a
unknown <bindhu.r2009@gmail.com>
2025-05-16 14:26:09 +0530

    what i did

--- pythonstrings.py
+++ pythonstrings.py
@@ -139,5 +139,38 @@
 # txt = f"The price is {20 * 59} dollars" "*multiply"
 # print(txt)
 
+# Escape Character "\"
+
+# txt = "We are the so-called \"Vikings\" from the north."
+# print(txt)
 
 
+# \'	Single Quote
+# txt = 'It\'s alright.'
+# print(txt) 	
+# \\	Backslash
+# txt = "This will insert one \\ (backslash)."
+# print(txt) 	
+# \n	New Line	
+# txt = "Hello\nWorld!"
+# print(txt) 
+# \r	Carriage Return	
+# txt = "Hello\rWorld!"
+# print(txt) 
+# \t	Tab	
+# txt = "Hello\tWorld!"
+# print(txt)
+# \b	Backspace	
+#This example erases one character (backspace):
+# txt = "Hello \bWorld!"
+# print(txt) 
+# \f	Form Feed	
+# move to new page or erase the screen
+# \ooo	Octal value	
+#A backslash followed by three integers will result in a octal value:
+# txt = "\110\145\154\154\157"
+# print(txt)                    - doubt (not understood what it does or is used for)
+# \xhh	Hex value
+# A backslash followed by an 'x' and a hex number represents a hex value:
+# txt = "\x48\x65\x6c\x6c\x6f"
+# print(txt)                  -same doubt
