3bca4c92d2a47fc525dd8e83e30d70acfd15fbec
unknown <bindhu.r2009@gmail.com>
2025-05-16 14:06:56 +0530

    what i did

--- pythonstrings.py
+++ pythonstrings.py
@@ -113,3 +113,31 @@
 # b = "World"
 # c = a + " " + b
 # print(c)
+
+# String Format
+
+# age = 36
+# txt = "My name is John, I am " + age
+# print(txt)
+
+# F-Strings
+
+# age = 36
+# txt = f"My name is John, I am {age}"
+# print(txt)
+
+# Placeholders and Modifiers
+
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars" "2 decimals"
+# print(txt)
+
+# txt = f"The price is {20 * 59} dollars" "*multiply"
+# print(txt)
+
+
+
