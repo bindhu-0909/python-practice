2f8e07bf540b2ff532d88d3f05e7058156bd15ab
unknown <bindhu.r2009@gmail.com>
2025-05-17 20:22:06 +0530

    what i did

---
+++ pythonoperators.py
@@ -0,0 +1,42 @@
+# Python Operators
+# print(10 + 5)
+
+# Python Arithmetic Operators
+
+# +	Addition	x + y	
+# x = 5
+# y = 3
+# print(x + y)
+
+# -	Subtraction	x - y	
+# x = 5
+# y = 3
+# print(x - y)
+
+# *	Multiplication	x * y	
+# x = 5
+# y = 3
+# print(x * y)
+
+# /	Division	x / y	
+# x = 12
+# y = 3
+# print(x / y)
+
+# %	Modulus	x % y	
+# x = 5
+# y = 2
+# print(x % y)
+
+# **	Exponentiation	x ** y	
+# x = 2
+# y = 5
+# print(x ** y) #same as 2*2*2*2*2
+
+# //	Floor division	x // y	
+# x = 15
+# y = 2
+# print(x // y)
+#the floor division // rounds the result down to the nearest whole numbe
+
+# Python Assignment Operators
