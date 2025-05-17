546fee00c0d7cf6bf7559ae532a7c482f6ef8b02
unknown <bindhu.r2009@gmail.com>
2025-05-17 19:59:36 +0530

    what i did

--- pythonbooleans.py
+++ pythonbooleans.py
@@ -13,3 +13,57 @@
 #   print("b is not greater than a")
 
 # Evaluate Values and Variables
+# print(bool("Hello"))
+# print(bool(0))
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+# Most Values are True
+# bool("abc")
+# bool(123)
+# bool(["apple", "cherry", "banana"])
+
+# Some Values are False
+# bool(False)
+# print(bool(False))
+# bool(None)
+# print(bool(None))
+# bool(0)
+# print(bool(0))
+# bool("")
+# print(bool(""))
+# bool(())
+# print(bool(()))
+# bool([])
+# print(bool([]))
+# bool({})
+# print(bool({}))
+
+# class myclass():
+#   def __len__(self):
+#     return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+# Functions can Return a Boolean
+# def myFunction() :
+#   return True
+
+# print(myFunction())
+
+# def myFunction() :
+#   return True
+
+# if myFunction():
+#   print("YES!")
+# else:
+#   print("NO!")
+
+# Check if an object is an integer or not:
+# x = 200
+# print(isinstance(x, int))
