7f52b559dbb6b6bef450d7a5a3cc8d43b3e53f04
unknown <bindhu.r2009@gmail.com>
2025-05-18 16:59:04 +0530

    what i did

--- pythonlists.py
+++ pythonlists.py
@@ -40,3 +40,9 @@
 # thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 # print(thislist)
 
+# Python Collections (Arrays)
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
