3f0ef9f3458cf85f8efb2e076c87906cb114258a
unknown <bindhu.r2009@gmail.com>
2025-05-18 16:48:54 +0530

    what i did

---
+++ pythonlists.py
@@ -0,0 +1,42 @@
+# Python Lists
+# List
+# Lists are used to store multiple items in a single variable.
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# List Items
+# List items are ordered, changeable, and allow duplicate values.
+
+# Ordered
+# When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+
+# Changeable
+# The list is changeable, meaning that we can change, add, and remove items in a list after it has been created
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# List Length
+# to determine how many items a list has, use the len() function:
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# List Items - Data Types
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+# print(list1)
+# print(list2)
+# print(list3)
+
+# list1 = ["abc", 34, True, 40, "male"]
+# print(list1)
+
+# type()
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+# The list() Constructor
+# thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thislist)
+
