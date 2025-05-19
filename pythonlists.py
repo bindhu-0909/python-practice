d0e74d10270413fde72dab9ed40f3207cb8eeff6
unknown <bindhu.r2009@gmail.com>
2025-05-19 14:35:52 +0530

    what i did

--- pythonlists.py
+++ pythonlists.py
@@ -77,3 +77,27 @@
 # thislist = ["apple", "banana", "cherry"]
 # if "apple" in thislist:
 #   print("Yes, 'apple' is in the fruits list")
+
+# Change Item Value
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# Change a Range of Item Values
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
