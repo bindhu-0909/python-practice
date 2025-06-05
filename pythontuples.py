e1f44d4221b15a5fa85bc821a2a114a984cc4696
unknown <bindhu.r2009@gmail.com>
2025-06-05 22:49:14 +0530

    what i did

---
+++ pythontuples.py
@@ -0,0 +1,40 @@
+# Tuple
+# Tuples are used to store multiple items in a single variable.
+# A tuple is a collection which is ordered and unchangeable.
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+
+# Tuple Items
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+
+# Unchangeable
+
+# Allow Duplicates
+# Since tuples are indexed, they can have items with the same value:
+
+# thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+# print(thistuple)
+
+# Tuple Length
+
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
+
+# Create Tuple With One Item
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+# #NOT a tuple
+# thistuple = ("apple")
+# print(type(thistuple))
+
+# Tuple Items - Data Types
+
+# tuple1 = ("apple", "banana", "cherry")
+# tuple2 = (1, 5, 7, 9, 3)
+# tuple3 = (True, False, False)
+# print(tuple1)
+# print(tuple2)
+# print(tuple3)
