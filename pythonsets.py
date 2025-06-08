fd1d8fa939f5b4b92125ec3a7f6e17bd694f4f26
unknown <bindhu.r2009@gmail.com>
2025-06-08 21:51:06 +0530

    what i did

---
+++ pythonsets.py
@@ -0,0 +1,51 @@
+# Set
+# Sets are used to store multiple items in a single variable
+
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+# Unordered
+# Unordered means that the items in a set do not have a defined order.
+
+# Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+
+# Unchangeable
+# Set items are unchangeable, meaning that we cannot change the items after the set has been created.
+
+# Duplicates Not Allowed
+
+# thisset = {"apple", "banana", "cherry", "apple"}
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry", False, True, 0}
+
+# print(thisset)
+
+# Get the Length of a Set
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print(len(thisset))
+
+# Set Items - Data Types
+
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {1, 5, 7, 9, 3}
+# set3 = {True, False, False}
+# print(set1)
+# print(set2)
+# print(set3)
+
+# set1 = {"abc", 34, True, 40, "male"}
+# print(set1)
+
+# myset = {"apple", "banana", "cherry"}
+# print(type(myset))
+
+# thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thisset)
